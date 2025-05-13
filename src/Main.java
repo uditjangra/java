3b0609387d78abca554ef282f312fcdbec427133
uditjangra <uditjangrax@gmail.com>
2025-05-13 13:23:21 +0530

    init commit

--- src/Main.java
+++ src/Main.java
@@ -13,14 +13,6 @@ public class Main {
         list.add(4);
         list.add(5);
         list.add(6);
-        list.add(7);
-        list.add(8);
-        list.add(9);
-        list.add(10);
-        list.add(11);
-        list.add(12);
-        list.add(13);
-        list.add(14);
         System.out.println(list);
     }
 }
