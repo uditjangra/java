b60ca296b7d62ece20189688fa9c0cf7f53dc79f
uditjangra <uditjangrax@gmail.com>
2025-05-13 13:00:26 +0530

    init commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+    }
+}
