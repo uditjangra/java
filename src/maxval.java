780b489fa46fba3108cd28ac7a5963bc7062ff91
uditjangra <uditjangrax@gmail.com>
2025-05-13 13:41:18 +0530

    init commit

---
+++ src/maxval.java
@@ -0,0 +1,15 @@
+public class maxval {
+    public static void main(String[] args) {
+        int[] arr = {1,4,5,68,123,5,12};
+        System.out.println(max(arr));
+    };
+    static int max(int[] arr) {
+        int max = arr[0];
+        for(int i=0;i<arr.length;i++) {
+            if(arr[i]>max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
---
+++ src/reverse.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class reverse {
+    public static void main(String[] args) {
+        int[] arr  = {1,2,3,4,5,2,32,5,3,12,3,};
+        reverse(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void reverse(int[] arr) {
+        int start;
+        int end  = arr.length-1;
+        for(start=0;start<arr.length/2;start++) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            end--;
+        }
+    }
+}
