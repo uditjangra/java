b106eb77f69418e248de0939621ec1e56b5a4fd1
uditjangra <uditjangrax@gmail.com>
2025-05-13 13:10:47 +0530

    init commit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,23 @@ import java.util.ArrayList;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<Integer>();
+        ArrayList<Integer> list = new ArrayList<>();
+//        slower than regular array
+//        In this you have to pass Wrapper Classes not Primitive Datatypes
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+        list.add(11);
+        list.add(12);
+        list.add(13);
+        list.add(14);
+        System.out.println(list);
     }
 }
